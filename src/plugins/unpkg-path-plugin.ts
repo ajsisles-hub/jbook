d8fa41b31b82b35ef7d278c11233d3ca09591479
Arielle Janine Sisles <ajsisles@yondu.com>
2026-08-13 13:00:10 +0800

    Added the input to the content of onLoad

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -15,7 +15,7 @@ const fileCache = localForage.createInstance({
 
 
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -50,10 +50,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-            import React, {useState} from 'react'; 
-            console.log(react);
-            `,
+            contents: inputCode,
           };
         }
 
