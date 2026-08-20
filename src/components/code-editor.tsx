22005139b78501e33670790d1b760a397eb4edc8
Arielle Janine Sisles <ajsisles@yondu.com>
2026-08-20 10:00:24 +0800

    Adding tab size and change event

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,18 +1,37 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { OnMount } from '@monaco-editor/react';
 
 
 interface CodeEditorProps {
   initialValue: string;
+  onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const handleEditorDidMount: OnMount = (editor) => {
+    console.log('Initial value:', editor.getValue());
+
+  };
+
+  const handleEditorChange = (value: string | undefined) => {
+    const updatedValue = value ?? '';
+    console.log('Current value:', updatedValue);
+    onChange(updatedValue);
+  };
+
+
+
+
   return (<MonacoEditor
     value={initialValue}
+    onChange={handleEditorChange}
+    onMount={handleEditorDidMount}
     height="500px"
     width="500px"
     theme="vs-dark"
     defaultLanguage='javascript'
     options={{
+      tabSize: 2,
+      insertSpaces: true,
       wordWrap: 'on',
       minimap: { enabled: false },
       showUnused: false,
--- src/index.tsx
+++ src/index.tsx
@@ -72,7 +72,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor initialValue='const a = 1;' />
+      <CodeEditor
+        initialValue='const a = 1;'
+        onChange={(value) => setInput(value)}
+      />
       <textarea value={input} onChange={onChangeEvent}>
       </textarea>
       <div>
