c133c4b6e4303c88d04737e2c1d06cb8f5c513b2
Arielle Janine Sisles <ajsisles@yondu.com>
2026-08-20 09:30:45 +0800

    Added monaco-editor and updates in code editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,28 @@
 import MonacoEditor from '@monaco-editor/react';
 
 
-const CodeEditor = () => {
-  return <MonacoEditor height="500px" width="500px" />
+interface CodeEditorProps {
+  initialValue: string;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+  return (<MonacoEditor
+    value={initialValue}
+    height="500px"
+    width="500px"
+    theme="vs-dark"
+    defaultLanguage='javascript'
+    options={{
+      wordWrap: 'on',
+      minimap: { enabled: false },
+      showUnused: false,
+      folding: false,
+      fontSize: 16,
+      lineNumbersMinChars: 3,
+      scrollBeyondLastLine: false,
+      automaticLayout: true
+    }}
+  />)
 };
 
 export default CodeEditor;
