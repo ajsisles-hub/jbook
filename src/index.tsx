5a83971f41b4e69e0cac135736d6d0e7a70a9647
Arielle Janine Sisles <ajsisles@yondu.com>
2026-08-20 15:44:38 +0800

    refactor:Make a CodeCell compoenent for editor and preview

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,12 @@
-import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import bundle from './bundler';
-
-import CodeEditor from './components/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import Preview from './components/preview';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-
-  const onChangeEvent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.target.value);
-  };
-
-  // bundling process
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-    console.log(output);
-  };
-
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 1;'
-        onChange={(value) => setInput(value)}
-      />
-      <textarea value={input} onChange={onChangeEvent}>
-      </textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>);
 };
 
