c5f60983fdf23e979886edee28b391907cd54710
Arielle Janine Sisles <ajsisles@yondu.com>
2026-08-26 14:05:51 +0800

    feat(action-reducer): add handling for MOVE_CELL, UPDATE_CELL, INSERT_CELL_BEFORE  and DELETE_CELL

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -30,20 +30,59 @@ const reducer = produce((
   action: Action) => {
 
   switch (action.type) {
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
+    case ActionType.INSERT_CELL_BEFORE: {
+      const { id } = action.payload;
+      const cell: Cell = {
+        type: action.payload.type,
+        id: randomId(),
+        content: '',
 
-    case ActionType.UPDATE_CELL:
+      };
+
+
+      // create cell
+      state.data[cell.id] = cell;
+
+      // find index
+      const index = state.order.findIndex(cellId => cellId === id);
+
+      // set order of cell
+      if (index < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(index, 0, cell.id);
+      }
+
+      return state;
+    }
+
+    case ActionType.UPDATE_CELL: {
       const { id, content } = action.payload;
       state.data[id].content = content;
       return state;
+    }
 
-
-    case ActionType.DELETE_CELL:
+    case ActionType.DELETE_CELL: {
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload)
       return state;
+    }
 
-    case ActionType.MOVE_CELL:
+    case ActionType.MOVE_CELL: {
+      const { id, direction } = action.payload;
+      const index = state.order.findIndex((cellId) => cellId === id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      //validate prevent out of bound
+      if (targetIndex < 0 || targetIndex >= state.order.length) {
+        return state;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = id;
+
       return state;
+    }
 
     default:
       return state;
@@ -51,6 +90,10 @@ const reducer = produce((
 
 
 });
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
+};
 
 export default reducer;
 
